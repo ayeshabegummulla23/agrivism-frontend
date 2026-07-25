3f08b81701b6a7c47e25997101d7a01ef3e7ac5d
Ayesha <ayeshabegummulla23@gmail.com>
2026-07-25 21:04:52 +0530

    fix: add BrowserRouter basename for GitHub Pages base path

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/agrivism-frontend">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
